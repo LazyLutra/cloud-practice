7504176764414902e8ef1705b64efa379d93ac9b
lazylutra@gmail.com <lazylutra@gmail.com>
2025-05-03 11:04:18 +0800

    项目初始化，集成nacos

---
+++ service-order/src/main/java/com/example/order/OrderApplication.java
@@ -0,0 +1,18 @@
+package com.example.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description: 订单服务启动类
+ * @author: huy
+ * @date: 2025/5/2 16:42
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+}
